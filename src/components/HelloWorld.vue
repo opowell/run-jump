a25060890570b31b8558587af3e2aa2a128634f8
opowell <opowell@gmail.com>
2023-11-01 11:34:36 +0100

    jumping

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,30 +4,46 @@ import { computed, ref } from 'vue'
 const count = ref(0)
 setInterval(() => {
   count.value = count.value + 1
+  console.log(playerPosition.value, playerJumpSpeed.value)
+  if (playerPosition.value > 5) {
+    playerJumpSpeed.value = playerJumpSpeed.value - 3
+  }
+  playerPosition.value = Math.max(playerPosition.value + playerJumpSpeed.value, 5)
+  if (playerPosition.value === 5) {
+    playerJumpSpeed.value = 0
+  }
 }, 100)
 
 const playerPosition = ref(5)
+const playerJumpSpeed = ref(0)
 const blocks = ref([200, 450])
 const playerStyle = computed(() => {
   return {
     bottom: playerPosition.value + 'px'
   }
 })
+function jump() {
+  if (playerPosition.value === 5) {
+    playerJumpSpeed.value = 15
+  }
+}
 </script>
 
 <template>
-  <div class="score">{{ count }}</div>
-  <div class="board">
-    <div class="character" :style="playerStyle" />
-    <div class="ground" />
-    <div class="block" v-for="(block, index) in blocks" :key="index" :style="{ left: block + 'px' }"/> 
+  <div class="game" @keydown.space="jump" tabindex="0">
+    <div class="score">{{ count }}</div>
+    <div class="board">
+      <div class="character" :style="playerStyle" />
+      <div class="ground" />
+      <div class="block" v-for="(block, index) in blocks" :key="index" :style="{ left: block + 'px' }"/> 
+    </div>
+    <h1>Run-Jump</h1>
   </div>
-  <h1>Run-Jump</h1>
 </template>
 
 <style scoped>
 .block {
-  height: 60px;
+  height: 30px;
   width: 30px;
   background-color: rgb(149, 6, 6);
   position: absolute;
@@ -41,7 +57,7 @@ const playerStyle = computed(() => {
 }
 .character {
   width: 40px;
-  height: 80px;
+  height: 70px;
   background-color: green;
   position: absolute;
 }
