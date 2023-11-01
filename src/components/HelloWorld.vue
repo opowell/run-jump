22661956603738bf42883202218780ce4c0b68fa
opowell <opowell@gmail.com>
2023-11-01 08:47:46 +0100

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="character" />
+  <div class="ground" />
+  <h1>Run-Jump</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+.character {
+  width: 40px;
+  height: 80px;
+  background-color: green;
+}
+.ground {
+  height: 5px;
+  width: 100%;
+  background-color: lightgray;
+}
+</style>
